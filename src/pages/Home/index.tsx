80b9b42e7d4fb72b401c1ed51c58e13d577eaf10
tuliomitico <tulioneedforspeed@gmail.com>
2025-08-23 12:07:40 -0300

    feat: improving inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,13 +18,25 @@ export function Home() {
           <TaskInput
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
             type="text"
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
+
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutos.</span>
         </FormContainer>
